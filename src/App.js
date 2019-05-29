2f6d4782a6624bdbf771f5f9ce93c10b1da81582
sirloinus <amaliesmidth@gmail.com>
2019-05-29 16:55:33 +0100

    added useState useReducer useRef useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import Toggle from './Toggle';
+import {useTitleInput} from './hooks/useTitleInput'
+import Counter from './Counter'
 
 const App = () => {
+
+  const [name, setName] = useTitleInput('')
+  const ref = useRef();
+
   return (
-    <div className="main-wrapper">
-      <h1>Level Up Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      
+      <h1 onClick={()=> ref.current.classList.add('hey-there')} >Level Up Dishes</h1>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        formSubmit(name, setName); 
+      }}>
+        <input type='text' onChange={(event) => setName(event.target.value)} value={name}/>
+        <button>Submit</button>
+      </form>
+
+      <h2>{name}</h2>
+
+      <Toggle/>
+      <Counter/>
     </div>
   );
 };
+
+const formSubmit = (value, setValue) => {
+
+  console.log(`sent to ${value}`)
+  setValue('')
+};
+
 
 export default App;
